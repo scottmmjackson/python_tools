b12972e7ad9c24716efaa3b0eced04f739a6628e
Scott Jackson <scottmmjackson@gmail.com>
2015-05-13 09:57:02 -0400

    some updates to make the script run more cleanly

--- is_pdf.py
+++ is_pdf.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python
 # is_pdf.py
-import pyPdf # pip install pyPdf to correct errors
 import argparse
 import sys
 import os
+try:
+   import pyPdf # pip install pyPdf to correct errors
+except ImportError:
+   sys.exit("pyPdf is required. If you don't have pyPdf, try 'pip install pyPdf'. If you don't have pip, go here: https://pypi.python.org/pypi/pip")
+if sys.version_info[0] != 2 or sys.version_info[1] < 7:
+   sys.exit("Python 2.7+ required. Got Python"+".".join(sys.version_info))
+
 p = argparse.ArgumentParser()
 p.add_argument("testFile",metavar="FILE",type=file,help="file to test for pdf or not")
 p.add_argument("-p","--posix",dest="posixReturn",action="store_true",help="Do not print anything, return true or false to shell")
@@ -16,4 +22,3 @@ try:
 except:
    if(args.posixReturn): sys.exit(os.EX_NOTFOUND)
    print args.testFile.name+": not a PDF"
-   raise
