2b3fb12b91f968bd23705c9544c87a2b54ab8264
Scott Jackson <scottmmjackson@gmail.com>
2015-05-21 19:59:58 -0400

    Now importable as a module!

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # python_tools
+
+A set of python tools that I've made to scratch itches.
+
+## is_pdf.py
+
+*Now with the ability to be imported as a module!*
+
+    >>> import is_pdf
+    >>> is_pdf.isPdfOpen("/Users/scottmmjackson/14-1799.Opinion.5-19-2015.1.PDF")
+    True
+    >>> is_pdf.isPdfOpen("/Users/scottmmjackson/14-1799.Opinion.5-19-2015.1.html")
+    False
+
+is_pdf.isPdf(filehandle) - takes a filehandle and returns true or false
+
+is_pdf.isPdfOpen(filename) - takes a fully qualified file name and passes the return value from isPdf
+
+For command-line usage, pass --help to is_pdf.py.
--- is_pdf.py
+++ is_pdf.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # is_pdf.py
-import argparse
 import sys
 import os
 try:
@@ -10,15 +9,30 @@ except ImportError:
 if sys.version_info[0] != 2 or sys.version_info[1] < 7:
    sys.exit("Python 2.7+ required. Got Python "+".".join(sys.version_info))
 
-p = argparse.ArgumentParser()
-p.add_argument("testFile",metavar="FILE",type=file,help="file to test for pdf or not")
-p.add_argument("-p","--posix",dest="posixReturn",action="store_true",help="Do not print anything, return true or false to shell")
-args = p.parse_args()
-try:
-   d = pyPdf.PdfFileReader(args.testFile)
-   assert d.getNumPages() > 0
-   if(args.posixReturn): sys.exit(os.EX_OK)
-   print args.testFile.name+": confirmed PDF"
-except:
-   if(args.posixReturn): sys.exit(os.EX_NOTFOUND)
-   print args.testFile.name+": not a PDF"
+if __name__ == '__main__':
+	import argparse
+	p = argparse.ArgumentParser()
+	p.add_argument("testFile",metavar="FILE",type=file,help="file to test for pdf or not")
+	p.add_argument("-p","--posix",dest="posixReturn",action="store_true",help="Do not print anything, return true or false to shell")
+	args = p.parse_args()
+	if(isPdf(args.testFile)):
+		if(args.posixReturn): 
+			sys.exit(os.EX_OK)
+		else: 
+			print args.testFile.name+": confirmed PDF"
+	else:
+		if(args.posixReturn): 
+			sys.exit(os.EX_NOTFOUND)
+		else:
+			print args.testFile.name+": not a PDF"
+
+def isPdfOpen(filename):
+	return isPdf(file(filename))
+
+def isPdf(filehandle):
+	try:
+	   d = pyPdf.PdfFileReader(filehandle)
+	   assert d.getNumPages() > 0
+	   return True
+	except:
+	   return False
